c83a97d80c9510156e4be5215a7cfada6b937ddd
sihan <sibbir.sihan.admissions@gmail.com>
2023-08-04 02:21:33 +0100

    11th commit

--- git_commands.ps1
+++ git_commands.ps1
@@ -1,3 +1,9 @@
+# Check if the commit message is provided as an argument
+if ($args.Count -eq 0) {
+    Write-Host "Please provide a commit message as an argument."
+    exit 1
+}
+
 # Change directory to your Git repository
 cd E:\programming\hadoop_hive_spark_docker
 
@@ -6,3 +12,5 @@ git status
 git add .
 git commit -m $args[0]
 git push
+
+# .\git_commands.ps1 "10th commit" 
